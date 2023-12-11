5e82d92741e3c7c06edd62c3016779d674eefc99
Eros Chan <soisyhpe@gmail.com>
2023-12-11 20:14:44 +0100

    feat: First Timer's css version

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,43 +1,182 @@
-import './Timer.css'
+import './timer.css';
+import {useRef, useState} from 'react';
 
-function Timer() {
+const formatTime = (time) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+};
 
+const Button = ({onClick, label, isDisabled}) => (
+  <button
+    onClick={onClick}
+    className={`w-full h-12 font-bold rounded-full text-4xl p-0 mr-1 ${
+      isDisabled ? 'bg-gray-400 text-white cursor-not-allowed' : 'bg-gray-900 text-white cursor-pointer'
+    }`}
+    disabled={isDisabled}
+  >
+    {label}
+  </button>
+);
+
+const ConfigTimerSection = ({title, getter, setter, maxValue, minValue, isTimer}) => {
+  const isMinValueReached = getter <= (isTimer ? minValue * 60 : minValue);
+  const isMaxValueReached = getter >= (isTimer ? maxValue * 60 : maxValue);
 
   return (
-    <div className='h-screen flex items-center justify-center'>
-
-      <div className='mb-12'>
-        <h1 className='text-4xl'>Working time</h1>
-        <p className='text-8xl'>25:00</p>
-        <div className='flex items-stretch'>
-          <button className='w-full h-12 rounded-full text-4xl p-0 mr-2'>-5</button>
-          <button className='w-full h-12 rounded-full text-4xl p-0 ml-2'>+5</button>
-        </div>
-      </div>
-
-      <div className='mb-12'>
-        <h1 className='text-4xl'>Break duration</h1>
-        <p className='text-8xl'>25:00</p>
-        <div className='flex items-stretch'>
-          <button className='w-full h-12 rounded-full text-4xl p-0 mr-2'>-5</button>
-          <button className='w-full h-12 rounded-full text-4xl p-0 ml-2'>+5</button>
-        </div>
-      </div>
-
-      <div className='mb-12'>
-        <h1 className='text-4xl'>Round</h1>
-        <p className='text-8xl'>4</p>
-        <div className='flex items-stretch'>
-          <button className='w-full h-12 rounded-full text-4xl p-0 mr-2'>-1</button>
-          <button className='w-full h-12 rounded-full text-4xl p-0 ml-2'>+1</button>
-        </div>
-      </div>
-
-      <div>
-        <button className='w-72 h-16 rounded-full text-4xl p-0'>Start</button>
+    <div className='w-72 mb-10'>
+      <h1 className='font-black text-4xl'>{title}</h1>
+      <p className='font-bold text-8xl'>{isTimer ? formatTime(getter) : getter}</p>
+      <div className='flex items-stretch'>
+        <Button
+          onClick={() =>
+            setter((previousValue) =>
+              Math.max(
+                previousValue - (isTimer ? minValue * 60 : minValue),
+                isTimer ? minValue * 60 : minValue
+              )
+            )
+          }
+          label={`-${minValue}`}
+          isDisabled={isMinValueReached}
+        />
+        <Button
+          onClick={() =>
+            setter((previousValue) =>
+              Math.min(
+                previousValue + (isTimer ? minValue * 60 : minValue),
+                isTimer ? maxValue * 60 : maxValue
+              )
+            )
+          }
+          label={`+${minValue}`}
+          isDisabled={isMaxValueReached}
+        />
       </div>
     </div>
-  )
+  );
+};
+
+const TimerSection = ({title, getter, isTimer}) => {
+  return (
+    <div className='w-72 mb-10'>
+      <h1 className='font-black text-4xl'>{title}</h1>
+      <p className='font-bold text-8xl'>{isTimer ? formatTime(getter) : getter}</p>
+    </div>
+  );
 }
 
-export default Timer
+function Timer() {
+  const [workingTime, setWorkingTime] = useState(25 * 60);
+  const [breakDuration, setBreakDuration] = useState(5 * 60);
+  const [round, setRound] = useState(4);
+
+  const [remainingTime, setRemainingTime] = useState(25 * 60)
+  const [nextBreakDuration, setNextBreakDuration] = useState(5 * 60)
+  const [roundCount, setRoundCount] = useState(1)
+
+  const [started, setStarted] = useState(false);
+
+  const [delayedStart, setDelayedStart] = useState(false);
+  const [countdown, setCountdown] = useState(3);
+
+  const startTimer = () => {
+    setStarted(!started);
+
+    // Add a 3-second delay before starting the timer
+    if (!started) {
+      setDelayedStart(true);
+
+      const countdownInterval = setInterval(() => {
+        setCountdown((prevCountdown) => {
+          if (prevCountdown === 1) {
+            clearInterval(countdownInterval);
+            setDelayedStart(false);
+
+            // Todo: Timer logic here
+            setStarted(true);
+          }
+          return prevCountdown - 1;
+        });
+      }, 1000);
+    } else {
+
+      // Todo: Timer logic here
+      setStarted(true);
+    }
+  };
+
+  return (
+    <div className='min-h-screen min-w-screen flex flex-col items-center justify-center'>
+
+
+
+      {delayedStart ? (
+        <p className='font-bold text-8xl mt-3'>
+          {countdown}
+        </p>
+      ) : (
+        <>
+          {!started && (
+            <>
+              <ConfigTimerSection
+                title='Working time'
+                getter={workingTime}
+                setter={setWorkingTime}
+                maxValue={60}
+                minValue={5}
+                isTimer={true}
+              />
+
+              {round > 1 && (
+                <ConfigTimerSection
+                  title='Break duration'
+                  getter={breakDuration}
+                  setter={setBreakDuration}
+                  maxValue={60}
+                  minValue={5}
+                  isTimer={true}
+                />
+              )}
+
+              <ConfigTimerSection title='Round' getter={round} setter={setRound} maxValue={4} minValue={1} isTimer={false}/>
+            </>
+          )}
+
+          {started && (
+            <>
+              <TimerSection
+                title='Remaining time'
+                getter={remainingTime}
+                isTimer={true}
+              />
+              {/*<TimerSection
+                title='Next break duration'
+                getter={nextBreakDuration}
+                isTimer={true}
+              />
+              <TimerSection
+                title='Round count'
+                getter={round}
+                isTimer={false}
+              />*/}
+            </>
+          )}
+
+          <div className='w-72'>
+            <button
+              onClick={startTimer}
+              className='bg-gray-900 text-white w-full h-16 rounded-full font-bold text-4xl p-0 m-0'
+            >
+              {started ? 'Pause' : 'Start'}
+            </button>
+          </div>
+        </>
+      )}
+
+
+    </div>
+  );
+}
+
+export default Timer;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Timer from './Timer.jsx'
+
 import './index.css'
+import Timer from './Timer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
